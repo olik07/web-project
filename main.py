8eaf569d811a3e11b6dccdce74cef473566879a0
olya <lobanova.olya07@yandex.ru>
2023-04-13 19:35:38 +0300

    autorisation

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template, redirect
+from flask_login import LoginManager, login_user, login_required, logout_user
 from data import db_session
 from data.users import User
 from forms.user import RegisterForm
+from forms.user_login import LoginForm
 
 app = Flask(__name__)
+login_manager = LoginManager()
+login_manager.init_app(app)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
@@ -36,6 +40,34 @@ def reqister():
         db_sess.commit()
         return redirect('/login')
     return render_template('register.html', title='Регистрация', form=form)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect('/')
 
 
 def main():
